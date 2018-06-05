fc11d933af1cbaa3f8314e6a876b2c3f008dd3e8
Michael Flanagin <flanna@uw.edu>
2018-06-05 16:28:54 -0700

    > Added 'Conditions' to easily modify simulation model parameters > Changed 'estimate regr. params' ExtendedMethods to MethodExtensions (to save actual names) > Added Phase 5: evaluate_metrics > Added phase 6: analyze_results > added phase 7: followup_analysis ** Added CAA with pbiasedalloc = 0.7 and 1.0 ** Only doing rerandomization with CAA methods

--- main.R
+++ main.R
@@ -6,6 +6,21 @@
 ## objective          = Main simulator file.
 
 library("simulator") # this file was created under simulator version 0.2.0
+# --------------------------------------------------------------------------- #
+#' ------------------------ [ 0. Conditions ] ------------------------------- #
+# --------------------------------------------------------------------------- #
+sim_trial_size = list( 32, 64, 96 )
+sim_outcome_type = c( "continuous" )
+sim_outcome_marginal_prevalence = c( 0.50 ) #' doesn't matter when Y continuous
+sim_prognostic_factor_type = c( "binary" ) # c("continuous", "binary"),
+sim_prognostic_factor_prevalence = list( 0.25, 0.50 )
+sim_prognostic_factor_number = list( 1, 2 )
+sim_prognostic_factor_effect_size = list( 1, 3 )
+sim_treatment_assignment_effect_size = list(1, 1.1, 3)
+sim_entry_time_effect_size = list( 1, 3 )
+sim_allocation_ratio = c( 0.50 )
+sim_num_rerandomizations = c( 500 )
+sim_alpha = c( 0.05 )
 
 # --------------------------------------------------------------------------- #
 #' -------------------------- [ I. Settings ] ------------------------------- #
@@ -15,7 +30,7 @@ simulation_name <- "alloc-model-AWS"
 results_directory <- "./results/"
 simulation_timestamp <- strftime(Sys.time(), format = "%Y-%m-%d_%H:%M") #' [ timestamp ]
 num_cores_parallel <- max(1, parallel::detectCores() - 1); #' [ for parallel computing ]
-num_simulations <- 10000; #' [ simulations per design matrix (all cores!)]
+num_simulations <- 100; #' [ simulations per design matrix (all cores!)]
 num_simulations_per_core <- ceiling( num_simulations / num_cores_parallel ); 
 num_reallocations <- 500; #' [ rerandomized allocations per simulated trial ]
 
@@ -23,9 +38,11 @@ num_reallocations <- 500; #' [ rerandomized allocations per simulated trial ]
 generate_model <- TRUE #' [ Phase 1: define all models, simulate draws ]
 allocate_groups <- TRUE #' [ Phase 2: simulate outcomes and allocate tx groups ]
 estimate_effects <- TRUE #' [ Phase 3: estimate tx effects (adjusted, unadjusted) ]
-estimate_rerandomized_errors <- TRUE #' [ Phase 4: estimate rerandomized errors (adjusted, unadjusted) ]
+estimate_rerandomized_errors <- FALSE #' [ Phase 4: estimate rerandomized errors (adjusted, unadjusted) ]
+evaluate_metrics <- TRUE #' [ Phase 5: evaluate metrics on output (TODO: don't run on 'alloc methods')]
 unadjusted_analyses <- FALSE #' [ (Phases 3,4) for now, ignore unadjusted analyses (TODO: complete!) ]
-analyze_results <- FALSE #' [ Phase 5: analyze results (tables and figures) ]
+analyze_results <- FALSE #' [ Phase 6: analyze results (tables and figures) ]
+followup_analysis <- FALSE #' [ Phase 7: add more detail to simulation ]
 
 source("model_functions.R") #' [ step 1: define your Model, its parameters, and simulation function (produces 'draws') ]
 source("method_functions.R") #' [ step 2: define your Methods, data analysis approaches (produces 'out') ]
@@ -38,23 +55,19 @@ source("eval_functions.R")#' [ step 3: define your Metrics, evaluation measures 
 #' [ Step 1: define treatment allocation Methods ]
 SR <- make_complete_randomization_with_outcomes()
 SBR <- make_stratified_block_randomization_with_outcomes()
-CAA <- make_covariate_adaptive_allocation_with_outcomes()
+CAA_deterministic <- make_covariate_adaptive_allocation_with_outcomes(allocation_biasing_probability = 1)
+CAA_probabilistic <- make_covariate_adaptive_allocation_with_outcomes(allocation_biasing_probability = 0.7)
 
-allocation_methods_list <- list( SR, SBR, CAA ); # pass these to ExtendedMethods (for tracking index:method)
-
-#' [ Step 2b: define ExtendedMethods to estimate tx effects ]
-get_adjusted_tx_effect <- lapply( allocation_methods_list, function( .alloc_method ){ 
-  estimate_regression_parameters( base_method = .alloc_method, adjusted = TRUE, return_extended_method = TRUE )})
-get_unadjusted_tx_effect <- lapply( allocation_methods_list, function( .alloc_method ){ 
-  estimate_regression_parameters( base_method = .alloc_method, adjusted = FALSE, return_extended_method = TRUE )})
-
-#' [ Step 3b: define ExtendedMethods to compute rerandomized std. error ests ]
-get_rerandomized_errors_adjusted_tx_effect <- lapply( allocation_methods_list, function( .alloc_method ){ 
-  rerandomized_error_estimates( base_method = .alloc_method, adjusted = TRUE, 
-                                num_rerandomizations = num_reallocations, return_extended_method = TRUE )})
-get_rerandomized_errors_unadjusted_tx_effect <- lapply( allocation_methods_list, function( .alloc_method ){ 
-  rerandomized_error_estimates( base_method = .alloc_method, adjusted = FALSE, 
-                                num_rerandomizations = num_reallocations, return_extended_method = TRUE )})
+#' [ Step 2b: define MethodExtensions to estimate tx effects ]
+adjusted_ests <- estimate_regression_parameters( adjusted = TRUE, return_extended_method = FALSE )
+unadjusted_ests <-  estimate_regression_parameters( adjusted = FALSE, return_extended_method = FALSE )
+#' [ Step 3b: define MethodExtensions to compute rerandomized std. error ests ]
+adjusted_ests_rerandomized <-   rerandomized_error_estimates( adjusted = TRUE, 
+                                                              num_rerandomizations = num_reallocations, 
+                                                              return_extended_method = FALSE )
+unadjusted_ests_rerandomized <-   rerandomized_error_estimates( adjusted = FALSE, 
+                                                              num_rerandomizations = num_reallocations, 
+                                                              return_extended_method = FALSE )
 
 # --------------------------------------------------------------------------- #
 #' ------------ [ III. Simulation design & evaluation ] --------------------- #
@@ -68,32 +81,29 @@ if( generate_model ){
                                label = "Randomization-allocation-methods-model",
                                dir = results_directory) %>%
     generate_model(make_trial_allocation_model,
-                   trial_size = list(32, 64, 96),
-                   outcome_type = c("binary"), # , "continuous"),
-                   outcome_marginal_prevalence = list( 0.10, 0.50 ),
-                   prognostic_factor_type = c( "binary" ), # c("continuous", "binary"),
-                   prognostic_factor_prevalence = list( 0.25, 0.50 ),
-                   prognostic_factor_number = list( 1, 2 ),
-                   prognostic_factor_effect_size = list( 1, 3 ),
-                   treatment_assignment_effect_size = list(1, 1.1, 3),
-                   entry_time_effect_size = list( 1, 3 ),
-                   allocation_ratio = c( 0.50 ),
-                   num_rerandomizations = c( 500 ),
-                   #                 allocation_biasing_probability = c( 1.0 ),
-                   alpha = c( 0.05 ),
+                   trial_size = sim_trial_size,
+                   outcome_type = sim_outcome_type,
+                   outcome_marginal_prevalence = sim_outcome_marginal_prevalence,
+                   prognostic_factor_type = sim_prognostic_factor_type,
+                   prognostic_factor_prevalence = sim_prognostic_factor_prevalence,
+                   prognostic_factor_number = sim_prognostic_factor_number,
+                   prognostic_factor_effect_size = sim_prognostic_factor_effect_size,
+                   treatment_assignment_effect_size = sim_treatment_assignment_effect_size,
+                   entry_time_effect_size = sim_entry_time_effect_size,
+                   allocation_ratio = sim_allocation_ratio,
+                   num_rerandomizations = sim_num_rerandomizations,
+                   alpha = sim_alpha,
                    vary_along = c("trial_size",
-                                  "outcome_marginal_prevalence",
+                                  # "outcome_marginal_prevalence",
                                   "prognostic_factor_prevalence",
-                                  "prognostic_factor_number",
+                                  # "prognostic_factor_number",
                                   "prognostic_factor_effect_size",
                                   "treatment_assignment_effect_size",
                                   "entry_time_effect_size"
-                                  #                  "allocation_biasing_probability"
-                                  #                   "prognostic_factor_type",
-                                  #                   "allocation_ratio",
-                                  #                   "allocation_max_imbalance"
-                                  #                   "outcome_type",
-                                  #                   "effect_sizes",
+                                  # "prognostic_factor_type",
+                                  # "allocation_ratio",
+                                  # "allocation_max_imbalance"
+                                  #  "outcome_type"
                    )) %>%
     simulate_from_model(nsim = num_simulations_per_core,
                         index = 1:num_cores_parallel,
@@ -107,13 +117,8 @@ if( generate_model ){
 # --------------------------------------------------------------------------- #
 if( allocate_groups ){ 
   simulation <- run_method(object = simulation,
-                           methods = list( SR, SBR ),
+                           methods = list( SR, SBR, CAA_deterministic, CAA_probabilistic),
                            parallel = list( socket_names = num_cores_parallel ))
-  #' [ run CAA separately (may take a while) ]
-  simulation <- run_method(object = simulation,
-                           methods = list( CAA ),
-                           parallel = list( socket_names = num_cores_parallel ))
-  
 }
 
 # --------------------------------------------------------------------------- #
@@ -121,19 +126,17 @@ if( allocate_groups ){
 # --------------------------------------------------------------------------- #
 if( estimate_effects ){
   simulation <- run_method(object = simulation,
-                           methods = get_adjusted_tx_effect[1:2],
-                           parallel = list( socket_names = num_cores_parallel ))
-  #' [ run CAA separately (may take a while) ]
-  simulation <- run_method(object = simulation,
-                           methods = get_adjusted_tx_effect[3],
+                           methods = list( SR + adjusted_ests,
+                                           SBR + adjusted_ests,
+                                           CAA_deterministic + adjusted_ests,
+                                           CAA_probabilistic + adjusted_ests),
                            parallel = list( socket_names = num_cores_parallel ))
   if( unadjusted_analyses ){
     simulation <- run_method(object = simulation,
-                             methods = get_unadjusted_tx_effect[1:2],
-                             parallel = list( socket_names = num_cores_parallel ))
-    #' [ run CAA separately (may take a while) ]
-    simulation <- run_method(object = simulation,
-                             methods = get_unadjusted_tx_effect[3],
+                             methods = list( SR + unadjusted_ests,
+                                             SBR + unadjusted_ests,
+                                             CAA_deterministic + unadjusted_ests,
+                                             CAA_probabilistic + unadjusted_ests),
                              parallel = list( socket_names = num_cores_parallel ))
   }
 }
@@ -143,30 +146,108 @@ if( estimate_effects ){
 # --------------------------------------------------------------------------- #
 if( estimate_rerandomized_errors ){
   simulation <- run_method(object = simulation,
-                           methods = get_adjusted_tx_effect[1:2],
-                           parallel = list( socket_names = num_cores_parallel ))
-  #' [ run CAA separately (may take a while) ]
-  simulation <- run_method(object = simulation,
-                           methods = get_adjusted_tx_effect[3],
+                           methods = list( CAA_deterministic + adjusted_ests_rerandomized,
+                                           CAA_probabilistic + adjusted_ests_rerandomized ),
                            parallel = list( socket_names = num_cores_parallel ))
   if( unadjusted_analyses ){
     simulation <- run_method(object = simulation,
-                             methods = get_unadjusted_tx_effect[1:2],
-                             parallel = list( socket_names = num_cores_parallel ))
-    #' [ run CAA separately (may take a while) ]
-    simulation <- run_method(object = simulation,
-                             methods = get_unadjusted_tx_effect[3],
+                             methods = list( CAA_deterministic + unadjusted_ests_rerandomized,
+                                             CAA_probabilistic + unadjusted_ests_rerandomized ),
                              parallel = list( socket_names = num_cores_parallel ))
   }
 }
-  
+ 
+
 # --------------------------------------------------------------------------- #
-#' --------- [ Phase 5: analyze results (tables and figures) ] -------------- #
+#' -------------- [ Phase 5: evaluate metrics on output ] ------------------- #
+# --------------------------------------------------------------------------- #
+if( evaluate_metrics ){
+  simulation <- evaluate(object = simulation,
+                         metrics = list( coverage,
+                                         power_p_value,
+                                         power_ci,
+                                         bias,
+                                         mse ))
+}
+
+# --------------------------------------------------------------------------- #
+#' --------- [ Phase 6: analyze results (tables and figures) ] -------------- #
 # --------------------------------------------------------------------------- #
 if( analyze_results ){
-  #' [ todo: mock tables and figures here! ]
+  #' [ Subset evaluations by removing evals on 'allocation methods' - they are mostly NA ]
+  method_names <- unique( sapply( simulation@evals_refs[[1]], function( .object ){ .object@method_name }) )
+  useful_methods <- method_names[which(nchar( method_names ) > 48)]
+  baz <- subset_evals(evals( simulation ), method_names = useful_methods )
+  
+  tabulate_eval(object = baz,
+                method_names = useful_methods[c(1,4,2,5,3,6)],
+                metric_name = "coverage",
+                se_format = "None",
+                output_type = "latex",
+                format_args = list(nsmall=3, digits=3))
+  tabulate_eval(object = baz,
+                method_names = useful_methods[c(1,4,2,5,3,6)],
+                metric_name = "power_p_value",
+                se_format = "None",
+                output_type = "latex",
+                format_args = list(nsmall=3, digits=3))
+  tabulate_eval(object = baz,
+                method_names = useful_methods[c(1,4,2,5,3,6)],
+                metric_name = "power_ci",
+                se_format = "None",
+                output_type = "latex",
+                format_args = list(nsmall=3, digits=3))
+  tabulate_eval(object = baz,
+                method_names = useful_methods[c(1,4,2,5,3,6)],
+                metric_name = "bias",
+                se_format = "None",
+                output_type = "latex",
+                format_args = list(nsmall=3, digits=3))
 }
 
 
+# --------------------------------------------------------------------------- #
+#' -------- [ Phase 7: Repeat (specific) simulation conditions ] ------------ #
+# --------------------------------------------------------------------------- #
+
+if( followup_analysis ){
+  #' [ Get data frame of model parameters -- to easily locate model index corresp. to a particular configuration ]
+  muh_models <- load( simulation@model_refs )
+  params_by_model <- as.data.frame(t(sapply( muh_models, function( .model ){ .model@params[c(1:6, 11:16)]  })))
+  model_indexes_of_interest <- with( params_by_model, which( treatment_assignment_effect_size == 3 & 
+                                                               prognostic_factor_effect_size == 3 & 
+                                                               entry_time_effect_size == 1 &
+                                                               prognostic_factor_number == 2 & 
+                                                               prognostic_factor_prevalence == 0.5 &
+                                                               trial_size == 96 &
+                                                               outcome_marginal_prevalence == 0.5 ))
+  
+  sim2 <- subset_simulation( simulation, subset = model_indexes_of_interest )
+  
+  
+  #' need to generate base_method() output first
+  sim2 <- run_method(object = sim2,
+                     methods = list( SR, SBR, CAA),
+                     parallel = list( socket_names = num_cores_parallel ))
+  sim2 <- run_method(object = sim2,
+                     methods = list( SR + adjusted_ests,
+                                     SBR + adjusted_ests,
+                                     CAA + adjusted_ests ),
+                     parallel = list( socket_names = num_cores_parallel ))
+  sim2 <- run_method(object = sim2,
+                     methods = list( SR + adjusted_ests_rerandomized,
+                                     SBR + adjusted_ests_rerandomized,
+                                     CAA + adjusted_ests_rerandomized ),
+                     parallel = list( socket_names = num_cores_parallel ))
+  
+  sim2 <- evaluate(object = sim2,
+                   metrics = list( coverage,
+                                   power_p_value,
+                                   power_ci,
+                                   bias,
+                                   mse))  
+  method_names <- unique( sapply( sim2@evals_refs[[1]], function( .object ){ .object@method_name }) )
+  useful_methods <- method_names[which(nchar( method_names ) > 48)]
+}
 
 
