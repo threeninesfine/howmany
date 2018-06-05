7b4330d8474d222bfde0ac06502ce5c7a5e4d061
Michael Flanagin <flanna@uw.edu>
2018-06-05 16:32:17 -0700

    Work with contY-binX-big on: 1) fixing refs

--- manage_datasets.R
+++ manage_datasets.R
@@ -32,62 +32,84 @@ sink()
 #' -------------------------------------------------------------------------- #
 #' [ 3) Evaluate content of 'contY-binX-big': 169 models, 35 models with draws ]
 #' -------------------------------------------------------------------------- #
-resultsdir_path <-  "~/Downloads/MSThesis/expanded_datasets/contY-binX-big/results/"
+evaluate_this_simulation <- FALSE
+if( evaluate_this_simulation ){
+  resultsdir_path <-  "~/Downloads/MSThesis/expanded_datasets/contY-binX-big/results/"
+  
+  #' [4) Skip all the steps and make simulation with all files.]
+  sim <- get_simulation_with_all_files(dir = resultsdir_path)
+  
+  #' [1) Evaluate contents (get output, model name, etc )] 
+  contents_sim <- get_contents( dir = resultsdir_path )
+  
+  #' [2) Find indices such that it has draws, evals, output etc]
+  si_draw_length <- sapply( contents_sim$objects, function( .obj ) length(.obj$draws) )
+  si_has_draws <- which( si_draw_length > 0 )
+  
+  si_out_length <- sapply( contents_sim$objects, function( .obj ) length(.obj$out) )
+  si_has_out <- which( si_out_length > 0 )
+  
+  si_evals_length <- sapply( contents_sim$objects, function( .obj ) length(.obj$evals) )
+  si_has_evals <- which( si_evals_length > 0 )
+  
+  #' [3) Get model names with draws, evals, and output]
+  indices_with_data <- intersect( si_has_out, intersect( si_has_evals, si_has_draws ))
+  
+  simsub <- subset_simulation(sim, subset = indices_with_data )
+  simsub <- rename( simsub, name = "contY-binX-big-subset" ) 
+  simsub <- relabel( simsub, label = "Continuous Y - binary X with 35 models" ) 
+  save_simulation(simsub)
+  
+  #' add directory to all model refs, draws refs, output refs, and evals refs.
+  sapply( 1:length( simsub@model_refs ), function( .index ){ simsub@model_refs[[ .index ]]@dir <<- simsub@dir; })
+  sapply( 1:length( simsub@draws_refs ), function( .index ){ simsub@draws_refs[[ .index ]][[1]]@dir <<- simsub@dir; }) #' assuming only one draw
+  sapply( 1:length( simsub@output_refs ), function( .index.i ){ 
+    num_output_refs <- length( simsub@output_refs[[ .index.i ]] );
+    sapply( 1:num_output_refs, function( .index.j ){ 
+      simsub@output_refs[[ .index.i ]][[ .index.j ]]@dir <<- simsub@dir;})})
+  
+  sapply( 1:length( simsub@evals_refs ), function( .index.i ){ 
+    num_evals_refs <- length( simsub@evals_refs[[ .index.i ]] );
+    sapply( 1:num_evals_refs, function( .index.j ){ 
+      simsub@evals_refs[[ .index.i ]][[ .index.j ]]@dir <<- simsub@dir;})})
+  
+  #' load in the models, draws, and output.
+  muh_models <- load( simsub@model_refs )
+  #' muh_draws <- load( simsub@draws_refs )
+  
+  #' Look at what output references are available
+  methods_by_model <- sapply( simsub@output_refs, function( .outref.lvl1 ){
+    table( sapply( .outref.lvl1, function( .outref.lvl2 ){ .outref.lvl2@method_name }) )})
+  
+  #' find output for example scenario to load, but first lets look at parameters 
+  #' Look at parameter settings
+  params_by_model <- as.data.frame(t(sapply( muh_models, function( .model ){ .model@params[c(1:6, 11:16)]  })))
+  
+  #' [ Choosing model 34 to modify output ]
+  #' params_by_model[34, ]
+  #'    trial_size outcome_type outcome_marginal_prevalence prognostic_factor_number prognostic_factor_type prognostic_factor_prevalence num_rerandomizations alpha
+  # 34         96   continuous                         0.5                        2                 binary                          0.5                  500  0.05
+  # prognostic_factor_effect_size treatment_assignment_effect_size entry_time_effect_size allocation_ratio
+  # 34                             3                                3                      1              0.5
+  
+  muh_output <- load( simsub@output_refs[[34]][1:15] )
+}else{
+  sim <- load_simulation( name = "contY-binX-big-subset",
+                          dir = "~/Downloads/MSThesis/expanded_datasets/contY-binX-big/results/" )
+  
+  sapply( 1:length( sim@model_refs ), function( .index ){ sim@model_refs[[ .index ]]@dir <<- sim@dir; })
+  sapply( 1:length( sim@draws_refs ), function( .index ){ sim@draws_refs[[ .index ]][[1]]@dir <<- sim@dir; }) #' assuming only one draw
+  sapply( 1:length( sim@output_refs ), function( .index.i ){ 
+    num_output_refs <- length( sim@output_refs[[ .index.i ]] );
+    sapply( 1:num_output_refs, function( .index.j ){ 
+      sim@output_refs[[ .index.i ]][[ .index.j ]]@dir <<- sim@dir;})})
+  
+  sapply( 1:length( sim@evals_refs ), function( .index.i ){ 
+    num_evals_refs <- length( sim@evals_refs[[ .index.i ]] );
+    sapply( 1:num_evals_refs, function( .index.j ){ 
+      sim@evals_refs[[ .index.i ]][[ .index.j ]]@dir <<- sim@dir;})})
+  
+  adj_ests <- load( sim@output_refs[[34]][1:15] )
+  allocs <- load( sim@output_refs[[34]][16:30] )
+}
 
-#' [4) Skip all the steps and make simulation with all files.]
-sim <- get_simulation_with_all_files(dir = resultsdir_path)
-
-#' [1) Evaluate contents (get output, model name, etc )] 
-contents_sim <- get_contents( dir = resultsdir_path )
-
-#' [2) Find indices such that it has draws, evals, output etc]
-si_draw_length <- sapply( contents_sim$objects, function( .obj ) length(.obj$draws) )
-si_has_draws <- which( si_draw_length > 0 )
-
-si_out_length <- sapply( contents_sim$objects, function( .obj ) length(.obj$out) )
-si_has_out <- which( si_out_length > 0 )
-
-si_evals_length <- sapply( contents_sim$objects, function( .obj ) length(.obj$evals) )
-si_has_evals <- which( si_evals_length > 0 )
-
-#' [3) Get model names with draws, evals, and output]
-indices_with_data <- intersect( si_has_out, intersect( si_has_evals, si_has_draws ))
-
-simsub <- subset_simulation(sim, subset = indices_with_data )
-simsub <- rename( simsub, name = "contY-binX-big-subset" ) 
-simsub <- relabel( simsub, label = "Continuous Y - binary X with 35 models" ) 
-save_simulation(simsub)
- 
-#' add directory to all model refs, draws refs, output refs, and evals refs.
-sapply( 1:length( simsub@model_refs ), function( .index ){ simsub@model_refs[[ .index ]]@dir <<- simsub@dir; })
-sapply( 1:length( simsub@draws_refs ), function( .index ){ simsub@draws_refs[[ .index ]][[1]]@dir <<- simsub@dir; }) #' assuming only one draw
-sapply( 1:length( simsub@output_refs ), function( .index.i ){ 
-  num_output_refs <- length( simsub@output_refs[[ .index.i ]] );
-  sapply( 1:num_output_refs, function( .index.j ){ 
-    simsub@output_refs[[ .index.i ]][[ .index.j ]]@dir <<- simsub@dir;})})
-
-sapply( 1:length( simsub@evals_refs ), function( .index.i ){ 
-  num_evals_refs <- length( simsub@evals_refs[[ .index.i ]] );
-  sapply( 1:num_evals_refs, function( .index.j ){ 
-    simsub@evals_refs[[ .index.i ]][[ .index.j ]]@dir <<- simsub@dir;})})
-
-#' load in the models, draws, and output.
-muh_models <- load( simsub@model_refs )
-#' muh_draws <- load( simsub@draws_refs )
-
-#' Look at what output references are available
-methods_by_model <- sapply( simsub@output_refs, function( .outref.lvl1 ){
-  table( sapply( .outref.lvl1, function( .outref.lvl2 ){ .outref.lvl2@method_name }) )})
-
-#' find output for example scenario to load, but first lets look at parameters 
-#' Look at parameter settings
-params_by_model <- as.data.frame(t(sapply( muh_models, function( .model ){ .model@params[c(1:6, 11:16)]  })))
-
-#' [ Choosing model 34 to modify output ]
-#' params_by_model[34, ]
-#'    trial_size outcome_type outcome_marginal_prevalence prognostic_factor_number prognostic_factor_type prognostic_factor_prevalence num_rerandomizations alpha
-# 34         96   continuous                         0.5                        2                 binary                          0.5                  500  0.05
-# prognostic_factor_effect_size treatment_assignment_effect_size entry_time_effect_size allocation_ratio
-# 34                             3                                3                      1              0.5
-
-muh_output <- load( simsub@output_refs[[34]][1:15] )
