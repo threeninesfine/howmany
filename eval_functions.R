cec14cdf203e7b81004cefe91f450839bed07e88
Michael Flanagin <flanna@uw.edu>
2018-06-13 14:41:04 -0700

    power_rerand(..): power is if observed treatment effect 'out$est' lies outside the quantile-based rerandomization confidence interval 'out$cilower' to 'out$ciupper' note: 'out$rerandomized' should be 1/TRUE, will throw an error if metric is passed output from Method output without '$rerandomized'

--- eval_functions.R
+++ eval_functions.R
@@ -7,15 +7,16 @@ power_p_value <- new_metric("power_p_value", "Power by P-value", metric = functi
   return(( out$p < model$alpha ))})
 
 power_ci <- new_metric("power_ci", "Power by CI", metric = function( model, out ){
-  if( out$rerandomized ){
-    return(( out$cilower <= out$est ) && ( out$est <= out$ciupper ))
-  }else{
-    return(( 0 < out$cilower ) | ( 0 > out$ciupper ))}})
+  return(( 0 < out$cilower ) | ( 0 > out$ciupper ))})
+
+power_rerand <- new_metric("power_rerand", "Power by rerandomized CI", metric = function( model, out ){
+  return(( out$cilower <= out$est ) && ( out$est <= out$ciupper ))})
 
 bias <- new_metric("bias", "Bias", metric = function(model, out){
   return( model$bZ - out$est ) })
 
 mse <- new_metric("mse", "MSE", metric = function(model, out){
   return(( model$bZ - out$est )^2) })
+
 
 
